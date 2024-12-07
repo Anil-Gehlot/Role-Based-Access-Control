fa1ff4a6c7d311c3e6dcadd2098424efb8ab048b
anil-gehlot <anilgehlot2000@gmail.com>
2024-12-07 10:23:16 +0530

    Rendering component according to Routes

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import { RBACProvider } from './context/RBACContext';
+import Login from './components/Login';
+import Dashboard from './components/Dashboard';
+import UserManagement from './components/UserManagement';
+import RoleManagement from './components/RoleManagement';
+import ProtectedRoute from './components/ProtectedRoute';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <AuthProvider>
+      <RBACProvider>
+        <Router>
+          <div className="min-h-screen bg-gray-100">
+            <Navbar />
+            <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+              <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/dashboard" element={
+                  <ProtectedRoute>
+                    <Dashboard />
+                  </ProtectedRoute>
+                } />
+                <Route path="/users" element={
+                  <ProtectedRoute requiredPermission="users.view">
+                    <UserManagement />
+                  </ProtectedRoute>
+                } />
+                <Route path="/roles" element={
+                  <ProtectedRoute requiredPermission="roles.manage">
+                    <RoleManagement />
+                  </ProtectedRoute>
+                } />
+                <Route path="/" element={<Navigate to="/dashboard" replace />} />
+              </Routes>
+            </div>
+          </div>
+        </Router>
+      </RBACProvider>
+    </AuthProvider>
+  );
+}
+
+export default App;
